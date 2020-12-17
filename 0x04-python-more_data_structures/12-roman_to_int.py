eb1a42cd1bcfc3e8a937d4e8d5d9b648375404b2
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-12-17 18:08:31 +0000

    Add task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    p = 0
+
+    for c in range(len(roman_string) - 1, -1, -1):
+        if val[roman_string[c]] >= p:
+            res += val[roman_string[c]]
+        else:
+            res -= val[roman_string[c]]
+        p = val[roman_string[c]]
+    return res
