fc7a6e4e887ed64bfe3f920ee817f5ce7c0557ff
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-12-17 20:36:33 +0000

    Fix 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-   for k, v in list(a_dictionary.items()):
+    for k, v in list(a_dictionary.items()):
         if v is value:
             a_dictionary.pop(k)
     return a_dictionary
