440b60ef33d1f186a7fe4b73ef6513e5dbf1265c
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-01-04 13:39:35 +0000

    Fix 103

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -46,18 +46,17 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	if (PyBytes_AsStringAndSize(p, &string, &size) != -1)
+	size = PyBytes_Size(p);
+	printf("  size: %zd\n", size);
+	string = (assert(PyBytes_Check(p)), (((PyBytesObject *)(p))->ob_sval));
+	printf("  trying string: %s\n", string);
+	printf("  first %zd bytes:", size < 10 ? size + 1 : 10);
+	while (i < size + 1 && i < 10)
 	{
-		printf("  size: %zd\n", size);
-		printf("  trying string: %s\n", string);
-		printf("  first %zd bytes:", size < 10 ? size + 1 : 10);
-		while (i < size + 1 && i < 10)
-		{
-			printf(" %02hhx", string[i]);
-			i++;
-		}
-		printf("\n");
+		printf(" %02hhx", string[i]);
+		i++;
 	}
+	printf("\n");
 }
 
 /**
