1b3c3830acd3569ec15ce1e329d00e30249f637e
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-01-20 18:43:53 +0000

    Fix 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""Log parsing script."""
+"""Script that reads stdin line by line and computes metrics"""
 import sys
 
-total_size = 0
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-iteration = 0
 
-
-def print_stats():
-    """Function that prints a resume of the stats."""
-    print("File size: {}".format(total_size))
-    for k, v in sorted(codes.items()):
-        if v is not 0:
+if __name__ == "__main__":
+    ordered = {}
+    size = i = 0
+    count = 1
+    data = sys.stdin
+    try:
+        for line in data:
+            i += 1
+            item = line.split(" ")
+            st_code = int(item[-2])
+            size += int(item[-1])
+            if ordered.get(st_code):
+                ordered[st_code] += count
+            else:
+                ordered[st_code] = count
+            if i % 10 == 0:
+                print("File size: {}".format(size))
+                for k, v in sorted(ordered.items()):
+                    print("{}: {}".format(k, v))
+    except KeyboardInterrupt:
+        print("File size: {}".format(size))
+        for k, v in sorted(ordered.items()):
             print("{}: {}".format(k, v))
-
-
-try:
-    for line in sys.stdin:
-        iteration += 1
-        line = line.split()
-
-        codes[int(line[7])] += 1
-        total_size += int(line[8])
-        if iteration % 10 == 0:
-            print_stats()
-
-except KeyboardInterrupt:
-    print_stats()
-    raise
+        raise
