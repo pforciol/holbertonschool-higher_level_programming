0173ed758fb95690d9c5f3b41f05f00b30e8b099
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-01-20 17:47:37 +0000

    Fix end='' in 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ Contains a function that reads a text file.
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r') as f:
-        print(f.read())
+        print(f.read(), end='')
