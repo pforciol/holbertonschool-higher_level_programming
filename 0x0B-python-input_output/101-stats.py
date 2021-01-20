c33be336a65b62762e967138f02ad5ac5070f94f
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-01-20 18:25:38 +0000

    Fix 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """Log parsing script."""
 import sys
+import signal
+
+total_size = 0
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+iteration = 1
 
 
-def print_stats(total_size, codes):
+def print_stats():
     print("File size: {}".format(total_size))
     for k, v in sorted(codes.items()):
         if v is not 0:
             print("{}: {}".format(k, v))
 
 
-total_size = 0
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-iteration = 0
-try:
-    for line in sys.stdin:
-        if iteration % 10 == 0 and iteration is not 0:
-            print_stats(total_size, codes)
+def signal_handler(sig, frame):
+    """Signal handler function."""
+    print_stats()
+    sys.exit()
 
-        codes[int(line.split(' ')[7])] += 1
-        total_size += int(line.split(' ')[8])
 
-        iteration += 1
+for line in sys.stdin:
+    line = line.split()
+    if iteration % 10 == 0:
+        print_stats()
 
-except KeyboardInterrupt:
-    print_stats(total_size, codes)
+    codes[int(line[7])] += 1
+    total_size += int(line[8])
+
+    iteration += 1
+    signal.signal(signal.SIGINT, signal_handler)
