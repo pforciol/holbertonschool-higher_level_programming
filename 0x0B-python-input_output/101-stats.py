e30a889e96c24b4c5ed4ea7418badb18192913b8
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-01-20 18:41:46 +0000

    Fix 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -17,13 +17,13 @@ def print_stats():
 
 try:
     for line in sys.stdin:
-        iteration += 1
         line = line.split()
-        if iteration % 10 == 0:
-            print_stats()
 
         codes[int(line[7])] += 1
         total_size += int(line[8])
+        if iteration % 10 == 0 and iteration is not 0:
+            print_stats()
+        iteration += 1
 
 except KeyboardInterrupt:
     print_stats()
