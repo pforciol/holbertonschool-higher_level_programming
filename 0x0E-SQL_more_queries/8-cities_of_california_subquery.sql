2cc3f430680e19c711f989825b8ca65ff15a35c3
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-02 11:18:15 +0000

    Add tasks 8 to 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Lists all the cities of California found in the database hbtn_0d_usa.
 
-SELECT `id`, `name` FROM `cities` WHERE `state_id` = (SELECT `id` FROM `states` WHERE `name` = "California") ORDER BY `state_id` ASC;
+SELECT `id`, `name` FROM `cities` WHERE `state_id` = (SELECT `id` FROM `states` WHERE `name` = "California") ORDER BY `id` ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Lists all the cities found in the database hbtn_0d_usa.
 
-SELECT `id`, `name` FROM `cities` ORDER BY `state_id` ASC;
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities` INNER JOIN `states` ON `cities`.`state_id` = `states`.`id` ORDER BY `cities`.`id` ASC;
