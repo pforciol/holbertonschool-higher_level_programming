5e96581b0a3dddaa885d3891985389252dd3a1ed
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-25 17:28:06 +0000

    Add task 11 of 0x0F

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that adds the `State` object “Louisiana”
+to the database `hbtn_0e_6_usa`.
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+
+    print(new.id)
