b829901259cbd850c1fecead8fe518720468a53a
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-25 15:51:40 +0000

    Add task 8 of 0x0F

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that prints the first `State` object from the database `hbtn_0e_6_usa`.
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    try:
+        instance = session.query(State).order_by(State.id).limit(1).all()[0]
+        print("{}: {}".format(instance.id, instance.name))
+    except:
+        print("Nothing")
