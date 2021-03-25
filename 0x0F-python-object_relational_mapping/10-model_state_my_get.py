6738a899d62d46091ace9ddf4802e13115b21329
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-25 17:19:16 +0000

    Add task 10 of 0x0F

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script that prints the `State` object in `hbtn_0e_0_usa`
+where `name` matches the argument `state name to search`.
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+    state name to search (str)
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    st_name = sys.argv[4]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mySQL_u, 'password': mySQL_p, 'database': db_name}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    q = session.query(State).filter(State.name == st_name).order_by(State.id)
+
+    try:
+        instance = q.all()[0]
+        print(instance.id)
+    except:
+        print("Not found")
