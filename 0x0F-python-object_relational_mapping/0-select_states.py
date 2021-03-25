8346454f569a052bc842f503fe8d66f2e758bd08
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-25 09:58:37 +0000

    Add READMEs and task 0 of 0x0F

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that lists all `states` from the database `hbtn_0e_0_usa`.
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # By default, it will connect to localhost:3306
+    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,9 @@
+# 0x0F - Python - Object-relational mapping
+
+At the end of this project, I am expected to be able to explain to anyone, without the help of Google:
+* Why Python programming is awesome
+* How to connect to a MySQL database from a Python script
+* How to `SELECT` rows in a MySQL table from a Python script
+* How to `INSERT` rows in a MySQL table from a Python script
+* What ORM means
+* How to map a Python Class to a MySQL table
