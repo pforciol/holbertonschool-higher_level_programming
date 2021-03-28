3f72aa3c30e5f93f6815444cf0055349dbb7b168
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-28 09:35:03 +0000

    Fix LIKE BINARY statement

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
 
     for row in rows:
